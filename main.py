24b404cea65084f411b59f350629ebe21ac3e993
Rayver Dasalla <rayverdasalla@gmail.com>
2024-08-22 11:47:34 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Dasalla, Rayver P.    BSCS301
+# 1st act
+# first_number = int(input("Enter first number: "))
+# second_number = int(input("Enter second number: "))
+#
+# if first_number > second_number:
+#     print(f"{first_number} is greater than {second_number}")
+# elif first_number < second_number:
+#     print(f"{first_number} is less than {second_number}")
+# elif first_number == second_number:
+#    print(f"{first_number} is equals to {second_number}")
+
+#2nd act
+classmate_1 = input("Enter classmate name 1: ")
+classmate_2 = input("Enter classmate name 2: ")
+classmate_3 = input("Enter classmate name 3: ")
+
+print(f"Your classmates are: {classmate_1}, {classmate_2}, & {classmate_3}")
